c16b2097d0d341a6c95612877a15161dc18ae171
atmansanjai <atmansanjai@gmail.com>
2026-08-15 12:27:03 +0530

    feat: login page ui

--- client/app/(application)/login/page.tsx
+++ client/app/(application)/login/page.tsx
@@ -1,8 +1,36 @@
-
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { Field, FieldGroup, FieldLabel } from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
 
 
 export default function LoginPage() {
   return (
-    <section>Login</section>
+    <section className={"flex items-center justify-center h-screen flex-col gap-5"}>
+      <Card className={"w-[400px]"}>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>Enter your ContactNumber below to login to your account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <FieldGroup>
+            <Field>
+              <FieldLabel>ContactNumber</FieldLabel>
+              <Input type="text" placeholder="Enter your ContactNumber" />
+            </Field>
+            <Field>
+              <FieldLabel>Password</FieldLabel>
+              <Input type="password" placeholder="Enter your Password" />
+            </Field>
+          </FieldGroup>
+        </CardContent>
+        <CardFooter className={"flex flex-col gap-3"}>
+          <Button className={"w-full"}>
+            Login
+          </Button>
+          <p>Don't have an account? Sign up</p>
+        </CardFooter>
+      </Card>
+    </section>
   )
 }
--- client/app/widgets/Sidebar/SidebarWidget.tsx
+++ client/app/widgets/Sidebar/SidebarWidget.tsx
@@ -1,10 +1,62 @@
-import { Sidebar, SidebarProvider } from '@/components/ui/sidebar'
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent,
+    SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton,
+    SidebarMenuItem, SidebarProvider } from '@/components/ui/sidebar'
+import Link from "next/link";
+
+interface sidebarMenus {
+    label: string;
+    href: string;
+}
+
 
 
 export default function SidebarWidget() {
+
+    const menus: sidebarMenus[] = [
+        {
+            label: "Admin",
+            href: "/dashboard/admin"
+        },
+        {
+            label: "Customer",
+            href: "/dashboard/customer"
+        },
+        {
+            label: "Milk",
+            href: "/dashboard/milk"
+        },
+        {
+            label: "Order",
+            href: "/dashboard/order"
+        }
+    ]
+
   return (
       <SidebarProvider>
         <Sidebar>
+            <SidebarHeader>
+                <p>Dairy management</p>
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Dashboard</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {
+                                menus.map((menu, index) => (
+                                    <SidebarMenuItem key={index}>
+                                            <Link href={menu.href}>
+                                                <SidebarMenuButton className={"relative"}>
+                                                {menu.label}
+                                        </SidebarMenuButton>
+                                            </Link>
+                                    </SidebarMenuItem>
+                                ))
+                            }
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
       </Sidebar>
       </SidebarProvider>
   )
